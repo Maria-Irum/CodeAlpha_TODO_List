864bca7e7722fd12c2f3155c424d9c5af2ac8dfd
Maria-Irum <144343712+Maria-Irum@users.noreply.github.com>
2023-10-31 20:34:29 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,75 @@
+document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+        var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i=0; i<tasks.length; i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+/* TO DO LIST */
+$(".tdl-new").bind('keypress', function(e){
+    var code = (e.keyCode ? e.keyCode : e.which);
+    if(code == 13) {
+      var v = $(this).val();
+      var s = v.replace(/ +?/g, '');
+      if (s == ""){
+        return false;
+      }else{
+        $(".tdl-content ul").append("<li><label><input type='checkbox'><i></i><span>"+ v +"</span><a href='#'>–</a></label></li>");
+        $(this).val("");
+      }
+    }
+  });
+
+
+  $(".tdl-content a").bind("click", function(){
+    var _li = $(this).parent().parent("li");
+        _li.addClass("remove").stop().delay(100).slideUp("fast", function(){
+          _li.remove();
+        });
+    return false;
+  });
+
+  // for dynamically created a tags
+  $(".tdl-content").on('click', "a", function(){
+    var _li = $(this).parent().parent("li");
+        _li.addClass("remove").stop().delay(100).slideUp("fast", function(){
+          _li.remove();
+        });
+    return false;
+  });
